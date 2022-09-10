6ddd0ab8dc884854a8811b4e7b078457aeaa2f80
twoosky <lhjae3@naver.com>
2022-09-11 02:02:36 +0900

    1.4.2 오브젝트 팩토리의 활용

--- src/main/java/user/dao/UserDaoTest.java
+++ src/main/java/user/dao/UserDaoTest.java
@@ -6,8 +6,13 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        /*
         ConnectionMaker connectionMaker = new DConnectionMaker();
         UserDao dao = new UserDao(connectionMaker);
+        */
+
+        // UserDao에서 어떤 ConnectionMaker 구현체를 사용할지 결정하는 기능을 팩토리 클래스로 분리
+        UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
         user.setId("whiteship");
